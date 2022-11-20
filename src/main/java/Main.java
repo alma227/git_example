5e7d80f27a8651b50682d2ae08b219e05fd9e3b4
alma227 <hapleness227@gmail.com>
2022-11-20 13:59:56 +0600

    conflict example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, qa.guru!");
-        System.out.println("My name is Almagul!");
+        System.out.println("Name [Censored]");
     }
 }
